ae974ddbf0130591a0833e3eb9235beadf57d7fd
hendisantika <hendisantika@yahoo.co.id>
2024-01-11 09:52:22 +0700

    Add Post Entity 🫸🌀✏️📗 :octocat:

---
+++ src/main/java/id/my/hendisantika/springbootelasticsearch/mode/entity/Post.java
@@ -0,0 +1,68 @@
+package id.my.hendisantika.springbootelasticsearch.mode.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-elasticsearch
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/11/24
+ * Time: 09:51
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "posts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post extends Base implements Serializable {
+
+    private static final long serialVersionUID = 7156526087883281623L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+    private String body;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    @JsonIgnoreProperties("postList")
+    private Author author;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "post_tags",
+            joinColumns = @JoinColumn(name = "post_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id"))
+    @JsonIgnoreProperties("postList")
+    private List<Tag> tagList;
+
+    public void addTag(Tag tag) {
+        this.tagList.add(tag);
+    }
+
+    public void removeTag(long tagId) {
+        Optional<Tag> tag = this.tagList.stream().filter(t -> t.getId() == tagId).findFirst();
+        tag.ifPresent(value -> this.tagList.remove(value));
+    }
+}
