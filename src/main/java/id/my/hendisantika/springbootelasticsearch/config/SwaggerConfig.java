1eb398a6e041bd968cc47ca0b7fc19156247a08e
hendisantika <hendisantika@yahoo.co.id>
2024-01-11 09:40:42 +0700

    Add apiDocConfig in SwaggerConfig 🫸🌀✏️📗 :octocat:

--- src/main/java/id/my/hendisantika/springbootelasticsearch/config/SwaggerConfig.java
+++ src/main/java/id/my/hendisantika/springbootelasticsearch/config/SwaggerConfig.java
@@ -1,6 +1,11 @@
 package id.my.hendisantika.springbootelasticsearch.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -28,5 +33,20 @@ public class SwaggerConfig implements WebMvcConfigurer {
         registry.addRedirectViewController("/", baseUrl.concat(REDIRECT_URL));
         registry.addRedirectViewController("/swagger-ui", baseUrl.concat(REDIRECT_URL));
         registry.addRedirectViewController("/api", baseUrl.concat(REDIRECT_URL));
+    }
+
+    @Bean
+    public OpenAPI apiDocConfig() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Spring Boot 3 Elasticsearch")
+                                .description("Spring Boot 3 Elasticsearch")
+                                .version("0.0.1")
+                                .contact(new Contact().name("hendisantika").email("hendisantika@yahoo.co.id")))
+                .externalDocs(
+                        new ExternalDocumentation()
+                                .description("Documentation")
+                                .url("https://s.id/hendisantika"));
     }
 }
