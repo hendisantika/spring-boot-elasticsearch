7f55de00303e44c037c771165e844e4a7c8d4366
hendisantika <hendisantika@yahoo.co.id>
2024-01-11 09:45:13 +0700

    Add ErrorResponse 🫸🌀✏️📗 :octocat:

---
+++ src/main/java/id/my/hendisantika/springbootelasticsearch/exception/base/ErrorResponse.java
@@ -0,0 +1,41 @@
+package id.my.hendisantika.springbootelasticsearch.exception.base;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-elasticsearch
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/11/24
+ * Time: 09:44
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class ErrorResponse {
+
+    /**
+     * error code
+     */
+    private String code;
+    /**
+     * short error message
+     */
+    private String message;
+
+    /**
+     * error cause timestamp
+     */
+    private Timestamp timestamp;
+}
