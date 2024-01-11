0c2d79c84892d4cb5268e44c6512e081fb710226
hendisantika <hendisantika@yahoo.co.id>
2024-01-11 09:48:07 +0700

    Add GlobalExceptionHandler 🫸🌀✏️📗 :octocat:

---
+++ src/main/java/id/my/hendisantika/springbootelasticsearch/exception/base/GlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package id.my.hendisantika.springbootelasticsearch.exception.base;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-elasticsearch
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/11/24
+ * Time: 09:47
+ * To change this template use File | Settings | File Templates.
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        Map<String, List<String>> body = new HashMap<>();
+
+        List<String> errors =
+                ex.getBindingResult().getFieldErrors().stream()
+                        .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                        .collect(Collectors.toList());
+
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+}
