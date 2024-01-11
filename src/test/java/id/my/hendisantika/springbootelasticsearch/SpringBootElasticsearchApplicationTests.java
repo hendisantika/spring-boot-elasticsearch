2bdb9ff95d15fec1e6de1ac1b1fe316a9779527e
hendisantika <hendisantika@yahoo.co.id>
2024-01-11 09:29:27 +0700

    Create starter project 🫸🌀✏️📗 :octocat:

---
+++ src/test/java/id/my/hendisantika/springbootelasticsearch/SpringBootElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootElasticsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
