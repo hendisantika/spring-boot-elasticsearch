4f1072c50d9f90d8e39a18134013517f2bd869f7
hendisantika <hendisantika@yahoo.co.id>
2024-01-11 09:41:14 +0700

    Add BadRequestException 🫸🌀✏️📗 :octocat:

---
+++ src/main/java/id/my/hendisantika/springbootelasticsearch/exception/BadRequestException.java
@@ -0,0 +1,28 @@
+package id.my.hendisantika.springbootelasticsearch.exception;
+
+import org.hibernate.service.spi.ServiceException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-elasticsearch
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/11/24
+ * Time: 09:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class BadRequestException extends ServiceException {
+
+    public BadRequestException() {
+        super();
+    }
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+    public BadRequestException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
