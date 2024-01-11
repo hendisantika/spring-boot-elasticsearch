76910eec92e59b02ccf4ee1c14ef30776bedc9e4
hendisantika <hendisantika@yahoo.co.id>
2024-01-11 09:39:41 +0700

    Add SwaggerConfig 🫸🌀✏️📗 :octocat:

---
+++ src/main/java/id/my/hendisantika/springbootelasticsearch/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.springbootelasticsearch.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-elasticsearch
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/11/24
+ * Time: 09:39
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    private static final String REDIRECT_URL = "/swagger-ui";
+
+    @Value("${spring.mvc.servlet.path}")
+    private String baseUrl;
+}
