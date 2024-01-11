fef537387753fc80dda18579e46c498911095b3d
hendisantika <hendisantika@yahoo.co.id>
2024-01-11 09:38:00 +0700

    Add messageSource in CustomLocaleResolver 🫸🌀✏️📗 :octocat:

--- src/main/java/id/my/hendisantika/springbootelasticsearch/config/CustomLocaleResolver.java
+++ src/main/java/id/my/hendisantika/springbootelasticsearch/config/CustomLocaleResolver.java
@@ -1,7 +1,9 @@
 package id.my.hendisantika.springbootelasticsearch.config;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
@@ -30,5 +32,14 @@ public class CustomLocaleResolver extends AcceptHeaderLocaleResolver implements 
         return headerLang == null || headerLang.isEmpty()
                 ? Locale.getDefault()
                 : Locale.lookup(Locale.LanguageRange.parse(headerLang), LOCALES);
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource rs = new ResourceBundleMessageSource();
+        rs.setBasename("messages/messages");
+        rs.setDefaultEncoding("UTF-8");
+        rs.setUseCodeAsDefaultMessage(true);
+        return rs;
     }
 }
