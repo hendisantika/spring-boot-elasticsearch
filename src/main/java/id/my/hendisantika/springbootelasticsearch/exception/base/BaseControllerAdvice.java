1273319a1f77c9aa3c9524cca33697aa632981be
hendisantika <hendisantika@yahoo.co.id>
2024-01-11 09:45:21 +0700

    Add noHandlerFoundException 🫸🌀✏️📗 :octocat:

--- src/main/java/id/my/hendisantika/springbootelasticsearch/exception/base/BaseControllerAdvice.java
+++ src/main/java/id/my/hendisantika/springbootelasticsearch/exception/base/BaseControllerAdvice.java
@@ -1,7 +1,11 @@
 package id.my.hendisantika.springbootelasticsearch.exception.base;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.sql.Timestamp;
 
@@ -20,4 +24,14 @@ import java.sql.Timestamp;
 public class BaseControllerAdvice {
 
     public static final Timestamp TIMESTAMP = new Timestamp(System.currentTimeMillis());
+
+    @ExceptionHandler({NoHandlerFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse noHandlerFoundException(NoHandlerFoundException ex) {
+        log.debug(ex.getMessage(), ex.getCause());
+        return new ErrorResponse(
+                String.valueOf(HttpStatus.NOT_FOUND.value()),
+                "No resource found for your request. Please verify you request",
+                TIMESTAMP);
+    }
 }
