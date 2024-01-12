7886b2859cc0093e425e9f5020705f039e1fbc39
hendisantika <hendisantika@yahoo.co.id>
2024-01-12 07:43:28 +0700

    Add createOrUpdate in AuthorEsController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootelasticsearch/controller/AuthorEsController.java
+++ src/main/java/id/my/hendisantika/springbootelasticsearch/controller/AuthorEsController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -41,4 +43,9 @@ public class AuthorEsController {
         return new ResponseEntity<>(entity, HttpStatus.OK);
     }
 
+    @PostMapping("/v1/es/authors")
+    public ResponseEntity<Author> createOrUpdate(@RequestBody Author author) {
+        Author updated = service.createOrUpdate(author);
+        return new ResponseEntity<>(updated, HttpStatus.OK);
+    }
 }
