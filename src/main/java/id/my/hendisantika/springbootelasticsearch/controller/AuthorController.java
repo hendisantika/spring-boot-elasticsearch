a25408a37ffd59ff982233cc595ebc48ef2478d1
hendisantika <hendisantika@yahoo.co.id>
2024-01-12 07:41:27 +0700

    Add getAuthorById in AuthorController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootelasticsearch/controller/AuthorController.java
+++ src/main/java/id/my/hendisantika/springbootelasticsearch/controller/AuthorController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -32,5 +33,11 @@ public class AuthorController {
     public ResponseEntity<List<Author>> getAllAuthors() {
         List<Author> list = service.getAllAuthors();
         return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/v1/authors/{id}")
+    public ResponseEntity<Author> getAuthorById(@PathVariable("id") Long id) {
+        Author entity = service.getById(id);
+        return new ResponseEntity<>(entity, HttpStatus.OK);
     }
 }
