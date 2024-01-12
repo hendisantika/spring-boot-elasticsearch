3f89614ee3c483375abbc1b83d683f388665ec2f
hendisantika <hendisantika@yahoo.co.id>
2024-01-12 07:36:43 +0700

    Add deleteById in AuthorService 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootelasticsearch/service/AuthorService.java
+++ src/main/java/id/my/hendisantika/springbootelasticsearch/service/AuthorService.java
@@ -1,8 +1,10 @@
 package id.my.hendisantika.springbootelasticsearch.service;
 
+import id.my.hendisantika.springbootelasticsearch.exception.BadRequestException;
 import id.my.hendisantika.springbootelasticsearch.exception.DataNotFoundException;
 import id.my.hendisantika.springbootelasticsearch.model.entity.Author;
 import id.my.hendisantika.springbootelasticsearch.repository.jpa.AuthorRepository;
+import id.my.hendisantika.springbootelasticsearch.util.Translator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -51,6 +53,15 @@ public class AuthorService {
             return authorRepository.save(authorUpdate);
         } else {
             return authorRepository.save(authorRequest);
+        }
+    }
+
+    public void deleteById(Long id) {
+        Optional<Author> author = authorRepository.findById(id);
+        if (author.isPresent()) {
+            authorRepository.deleteById(id);
+        } else {
+            throw new BadRequestException(Translator.toLocale("DELETE_ERROR_PLEASE_CHECK_ID"));
         }
     }
 }
