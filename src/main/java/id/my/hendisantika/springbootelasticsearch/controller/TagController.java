69c8c33a2cff9d064e21bd5f54d63f03e4130342
hendisantika <hendisantika@yahoo.co.id>
2024-01-12 07:47:04 +0700

    Add getAllTags in TagController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootelasticsearch/controller/TagController.java
+++ src/main/java/id/my/hendisantika/springbootelasticsearch/controller/TagController.java
@@ -1,9 +1,15 @@
 package id.my.hendisantika.springbootelasticsearch.controller;
 
+import id.my.hendisantika.springbootelasticsearch.model.entity.Tag;
 import id.my.hendisantika.springbootelasticsearch.service.TagService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +28,9 @@ public class TagController {
 
     private final TagService service;
 
+    @GetMapping("/v1/tags")
+    public ResponseEntity<List<Tag>> getAllTags() {
+        List<Tag> list = service.getAllTags();
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
 }
