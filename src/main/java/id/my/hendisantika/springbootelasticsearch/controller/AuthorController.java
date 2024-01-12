121e6ae8f468ffddf2d679e121acc683bfa87bc2
hendisantika <hendisantika@yahoo.co.id>
2024-01-12 07:41:58 +0700

    Add deleteById in AuthorController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootelasticsearch/controller/AuthorController.java
+++ src/main/java/id/my/hendisantika/springbootelasticsearch/controller/AuthorController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,5 +48,10 @@ public class AuthorController {
     public ResponseEntity<Author> createOrUpdate(@RequestBody Author Author) {
         Author updated = service.createOrUpdate(Author);
         return new ResponseEntity<>(updated, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/v1/authors/{id}")
+    public void deleteById(@PathVariable("id") Long id) {
+        service.deleteById(id);
     }
 }
