2b8d3a383d3a3b34bce5e408454d19c328eaa82c
hendisantika <hendisantika@yahoo.co.id>
2024-01-12 07:45:01 +0700

    Add deleteById in PostController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootelasticsearch/controller/PostController.java
+++ src/main/java/id/my/hendisantika/springbootelasticsearch/controller/PostController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,5 +51,10 @@ public class PostController {
     public ResponseEntity<Post> createOrUpdate(@Valid @RequestBody PostDTO postDTO) {
         Post updated = service.createOrUpdate(postDTO);
         return new ResponseEntity<>(updated, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/v1/posts/{id}")
+    public void deleteById(@PathVariable("id") Long id) {
+        service.deleteById(id);
     }
 }
