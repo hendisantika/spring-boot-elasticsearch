665ce983a35d0ba4565bd47c250ae781c183dff8
hendisantika <hendisantika@yahoo.co.id>
2024-01-12 07:44:48 +0700

    Add createOrUpdate in PostController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootelasticsearch/controller/PostController.java
+++ src/main/java/id/my/hendisantika/springbootelasticsearch/controller/PostController.java
@@ -1,13 +1,17 @@
 package id.my.hendisantika.springbootelasticsearch.controller;
 
+import id.my.hendisantika.springbootelasticsearch.model.dto.PostDTO;
 import id.my.hendisantika.springbootelasticsearch.model.entity.Post;
 import id.my.hendisantika.springbootelasticsearch.service.PostService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +44,11 @@ public class PostController {
     public ResponseEntity<Post> getPostById(@PathVariable("id") Long id) {
         Post entity = service.getById(id);
         return new ResponseEntity<>(entity, HttpStatus.OK);
+    }
+
+    @PostMapping("/v1/posts")
+    public ResponseEntity<Post> createOrUpdate(@Valid @RequestBody PostDTO postDTO) {
+        Post updated = service.createOrUpdate(postDTO);
+        return new ResponseEntity<>(updated, HttpStatus.OK);
     }
 }
