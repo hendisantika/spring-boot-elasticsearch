0f4bf982e36b8067ace1e0d008fff9ea366a9a62
hendisantika <hendisantika@yahoo.co.id>
2024-01-12 07:44:20 +0700

    Add getAllPosts in PostController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootelasticsearch/controller/PostController.java
+++ src/main/java/id/my/hendisantika/springbootelasticsearch/controller/PostController.java
@@ -1,9 +1,16 @@
 package id.my.hendisantika.springbootelasticsearch.controller;
 
+import id.my.hendisantika.springbootelasticsearch.model.entity.Post;
 import id.my.hendisantika.springbootelasticsearch.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +28,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     private final PostService service;
+
+    @GetMapping("/v1/posts")
+    public ResponseEntity<List<Post>> getAllPosts(@RequestParam(required = false) String title) {
+        List<Post> list = service.getAllPosts(title);
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
 }
