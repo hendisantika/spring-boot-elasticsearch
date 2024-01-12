579a8082517b217d871ccd3a25ad669f1a76b5ab
hendisantika <hendisantika@yahoo.co.id>
2024-01-12 07:36:27 +0700

    Add createOrUpdate in AuthorService 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootelasticsearch/service/AuthorService.java
+++ src/main/java/id/my/hendisantika/springbootelasticsearch/service/AuthorService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.text.MessageFormat;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -37,5 +38,19 @@ public class AuthorService {
                         () ->
                                 new DataNotFoundException(
                                         MessageFormat.format("Author id {0} not found", String.valueOf(id))));
+    }
+
+    public Author createOrUpdate(Author authorRequest) {
+        Optional<Author> existingAuthor = authorRepository.findById(authorRequest.getId());
+
+        if (existingAuthor.isPresent()) {
+            Author authorUpdate = existingAuthor.get();
+
+            authorUpdate.setName(authorRequest.getName());
+
+            return authorRepository.save(authorUpdate);
+        } else {
+            return authorRepository.save(authorRequest);
+        }
     }
 }
