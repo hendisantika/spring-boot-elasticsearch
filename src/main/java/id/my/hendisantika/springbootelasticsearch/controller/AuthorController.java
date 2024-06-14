6f6bd8b7712d4a51d2b3c33792c0e7458200f4b1
hendisantika <hendisantika@yahoo.co.id>
2024-06-14 07:02:05 +0700

    Add AuthorController 🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/springbootelasticsearch/controller/AuthorController.java
+++ src/main/java/id/my/hendisantika/springbootelasticsearch/controller/AuthorController.java
@@ -2,6 +2,11 @@ package id.my.hendisantika.springbootelasticsearch.controller;
 
 import id.my.hendisantika.springbootelasticsearch.model.entity.Author;
 import id.my.hendisantika.springbootelasticsearch.service.AuthorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,29 +33,98 @@ import java.util.List;
 @RestController
 @CrossOrigin
 @RequiredArgsConstructor
+@Tag(name = "Author", description = "Endpoint for managing Auth")
 public class AuthorController {
 
     private final AuthorService service;
 
     @GetMapping("/v1/authors")
+    @Operation(
+            summary = "Get All Author Data",
+            description = "Get All Author Data.",
+            tags = {"Author"})
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    description = "Success",
+                    responseCode = "200",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation =
+                            Author.class))
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Not found", responseCode = "404",
+                    content = @Content),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Internal error", responseCode = "500"
+                    , content = @Content)
+    }
+    )
     public ResponseEntity<List<Author>> getAllAuthors() {
         List<Author> list = service.getAllAuthors();
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @GetMapping("/v1/authors/{id}")
+    @Operation(
+            summary = "Get All Author Data by ID",
+            description = "Get All Author Data by ID.",
+            tags = {"Author"})
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    description = "Success",
+                    responseCode = "200",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation =
+                            Author.class))
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Not found", responseCode = "404",
+                    content = @Content),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Internal error", responseCode = "500"
+                    , content = @Content)
+    }
+    )
     public ResponseEntity<Author> getAuthorById(@PathVariable("id") Long id) {
         Author entity = service.getById(id);
         return new ResponseEntity<>(entity, HttpStatus.OK);
     }
 
     @PostMapping("/v1/authors")
+    @Operation(
+            summary = "Add New Author Data",
+            description = "Add New Author Data.",
+            tags = {"Author"})
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    description = "Success",
+                    responseCode = "200",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation =
+                            Author.class))
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Not found", responseCode = "404",
+                    content = @Content),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Internal error", responseCode = "500"
+                    , content = @Content)
+    }
+    )
     public ResponseEntity<Author> createOrUpdate(@RequestBody Author Author) {
         Author updated = service.createOrUpdate(Author);
         return new ResponseEntity<>(updated, HttpStatus.OK);
     }
 
     @DeleteMapping("/v1/authors/{id}")
+    @Operation(
+            summary = "Delete Author Data",
+            description = "Delete Author Data.",
+            tags = {"Author"})
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    description = "Success",
+                    responseCode = "200",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation =
+                            Author.class))
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Not found", responseCode = "404",
+                    content = @Content),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Internal error", responseCode = "500"
+                    , content = @Content)
+    }
+    )
     public void deleteById(@PathVariable("id") Long id) {
         service.deleteById(id);
     }
