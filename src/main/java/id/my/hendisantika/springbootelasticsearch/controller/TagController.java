8d430fae42733c4a066831583f38e9b60991f8e0
hendisantika <hendisantika@yahoo.co.id>
2024-06-14 07:02:47 +0700

    Add TagController 🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/springbootelasticsearch/controller/TagController.java
+++ src/main/java/id/my/hendisantika/springbootelasticsearch/controller/TagController.java
@@ -2,6 +2,10 @@ package id.my.hendisantika.springbootelasticsearch.controller;
 
 import id.my.hendisantika.springbootelasticsearch.model.entity.Tag;
 import id.my.hendisantika.springbootelasticsearch.service.TagService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,29 +32,98 @@ import java.util.List;
 @RestController
 @CrossOrigin
 @RequiredArgsConstructor
+@io.swagger.v3.oas.annotations.tags.Tag(name = "Tag", description = "Endpoint for managing Tag")
 public class TagController {
 
     private final TagService service;
 
     @GetMapping("/v1/tags")
+    @Operation(
+            summary = "Get All Tags Data",
+            description = "Get All Tags Data.",
+            tags = {"Post"})
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    description = "Success",
+                    responseCode = "200",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation =
+                            id.my.hendisantika.springbootelasticsearch.model.entity.Author.class))
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Not found", responseCode = "404",
+                    content = @Content),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Internal error", responseCode = "500"
+                    , content = @Content)
+    }
+    )
     public ResponseEntity<List<Tag>> getAllTags() {
         List<Tag> list = service.getAllTags();
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @GetMapping("/v1/tags/{id}")
+    @Operation(
+            summary = "Get Tag Data by ID",
+            description = "Get Tag Data by ID.",
+            tags = {"Post"})
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    description = "Success",
+                    responseCode = "200",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation =
+                            id.my.hendisantika.springbootelasticsearch.model.entity.Author.class))
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Not found", responseCode = "404",
+                    content = @Content),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Internal error", responseCode = "500"
+                    , content = @Content)
+    }
+    )
     public ResponseEntity<Tag> getTagById(@PathVariable("id") Long id) {
         Tag entity = service.getById(id);
         return new ResponseEntity<>(entity, HttpStatus.OK);
     }
 
     @PostMapping("/v1/tags")
+    @Operation(
+            summary = "Add New Tag Data",
+            description = "Add New Tag Data.",
+            tags = {"Post"})
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    description = "Success",
+                    responseCode = "200",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation =
+                            id.my.hendisantika.springbootelasticsearch.model.entity.Author.class))
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Not found", responseCode = "404",
+                    content = @Content),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Internal error", responseCode = "500"
+                    , content = @Content)
+    }
+    )
     public ResponseEntity<Tag> createOrUpdate(@RequestBody Tag Tag) {
         Tag updated = service.createOrUpdate(Tag);
         return new ResponseEntity<>(updated, HttpStatus.OK);
     }
 
     @DeleteMapping("/v1/tags/{id}")
+    @Operation(
+            summary = "Delete Tag Data",
+            description = "Delete Tag Data.",
+            tags = {"Post"})
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    description = "Success",
+                    responseCode = "200",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation =
+                            id.my.hendisantika.springbootelasticsearch.model.entity.Author.class))
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Not found", responseCode = "404",
+                    content = @Content),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Internal error", responseCode = "500"
+                    , content = @Content)
+    }
+    )
     public void deleteById(@PathVariable("id") Long id) {
         service.deleteById(id);
     }
