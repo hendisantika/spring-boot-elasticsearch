4240069c3d19a760b4a6cf9e4ba229127aa5499a
hendisantika <hendisantika@yahoo.co.id>
2024-12-11 05:53:00 +0700

    feat: Add Product🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/springbootelasticsearch/model/dto/Product.java
@@ -0,0 +1,59 @@
+package id.my.hendisantika.springbootelasticsearch.model.dto;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-elasticsearch
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/12/24
+ * Time: 05.52
+ * To change this template use File | Settings | File Templates.
+ */
+public class Product {
+    private String id;
+    private String name;
+    private String description;
+    private double price;
+    private int stockAvailable;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStockAvailable() {
+        return stockAvailable;
+    }
+
+    public void setStockAvailable(int stockAvailable) {
+        this.stockAvailable = stockAvailable;
+    }
+}
