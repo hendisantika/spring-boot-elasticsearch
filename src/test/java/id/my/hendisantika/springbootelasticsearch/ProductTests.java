82fc8951c16b0982c75924312a94564115056d44
hendisantika <hendisantika@yahoo.co.id>
2024-12-11 05:56:20 +0700

    feat: Add ProductTests🫸🌀✏️📗🐧🐳⬆

---
+++ src/test/java/id/my/hendisantika/springbootelasticsearch/ProductTests.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.springbootelasticsearch;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-elasticsearch
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/12/24
+ * Time: 05.56
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProductTests {
+    private final String json = "{" +
+            "\"name\":\"Best name ever\"," +
+            "\"description\":\"This is a wonderful description\"," +
+            "\"price\":123.32," +
+            "\"stock_available\":123" +
+            "}";
+
+}
